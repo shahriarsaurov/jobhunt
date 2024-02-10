6270b3403f2fbc4c3874fb8b066137fa02532072
shahriarsaurov <159621616+shahriarsaurov@users.noreply.github.com>
2024-02-10 12:37:27 +0100

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG') == 'True'
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '*']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '*', 'jobhunt-backend-225281cb5b3a.herokuapp.com/']
 
 
 # Application definition
